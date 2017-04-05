5bf1fce50c35f28593ee34bdc5627e5a9077657e
Robert Bue <robert@good-morning.no>
2017-04-05 13:57:20 +0200

    Added new structure

---
+++ MultipleComponentsOneStyleFile/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Wrapper, Header, Button } from './styles';
+
+export default function ModalBox () {
+  return (
+    <Wrapper>
+      <Header>Welcome</Header>
+      <Button>Button</Button>
+    </Wrapper>
+  );
+}
---
+++ MultipleComponentsOneStyleFile/styles.js
@@ -0,0 +1,9 @@
+import Wrapper from './Wrapper';
+import Header from './Header';
+import Button from './Button';
+
+export {
+  Wrapper,
+  Header,
+  Button
+};
